e65d7e97908db12989e0f56aff82378f432a4205
zekunjin <zekun.jin@foxmail.com>
2023-08-17 11:08:41 +0800

    chore: add openai embeddings

--- routes/embeddings.py
+++ routes/embeddings.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from utils.router import router
 from utils.llm import text_splitter
 
@@ -9,5 +11,7 @@ class EmbeddingsReq(BaseModel):
 
 @router.post("/embeddings")
 async def index(req: EmbeddingsReq):
-    splits = text_splitter.split_text(req.input)
-    return len(splits)
+    texts = text_splitter.split_text(req.input)
+    embedding = OpenAIEmbeddings()
+    Chroma.from_texts(texts, embedding).persist()
+    return len(texts)
